46bbd68243dd1b6c9bc0babc19cd65ac445ee4ea
sourav-manna <82703663+sourav-manna@users.noreply.github.com>
2022-07-06 17:40:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var num = Math.floor(Math.random(0,100)*100)+1
 var round = 0
 function CheckNumber(){
-    var guessnum = parseInt(document.getElementById("n").value)
-    if(guessnum == null || guessnum > 100 || guessnum < 1){
+    var k = document.getElementById("n").value
+    var guessnum = parseInt(k)
+    if(k == "" || guessnum > 100 || guessnum < 1){
         document.getElementById("p1").innerHTML = 'Please enter number between 1-100'
     }
     else{
